913407f1df990d7dbacf40a6429bcd4ccb71d778
Amaction <93982801+Amaction@users.noreply.github.com>
2021-11-19 13:37:49 +0700

    upload

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require('express');
 
 const server = express();
 
-server.use(express.static(__dirname + '/'));
+server.use('/', express.static(__dirname + '/public'));
+server.use('/3D-Work', express.static(__dirname + '/public'));
 server.listen(8080);
