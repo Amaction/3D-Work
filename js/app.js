ee474d9926d87487db282e57352b5e1341b6bfe2
Amaction <93982801+Amaction@users.noreply.github.com>
2021-11-19 14:10:17 +0700

    upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
+lowLag.init();
+lowLag.load("/sound/yay.mp3");
+lowLag.load("/sound/jump.mp3");
+lowLag.load("/sound/kick.mp3");
+lowLag.load("/sound/clap.mp3");
+lowLag.load("/sound/blow.mp3");
+lowLag.load("/sound/walk.mp3");
+
+let pre_Sec = 0;
+
+// Scene
 const scene = new THREE.Scene();
 
 scene.background = new THREE.Color( 0x808080 );
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
+
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -36,8 +50,8 @@ const loader = new THREE.GLTFLoader();
 let campos = findGetParameter('campos').split(',');
 camera.position.set( parseInt(campos[0]), parseInt(campos[1]), parseInt(campos[2]) );
 
-
 controls.update();
+/*
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
@@ -45,10 +59,108 @@ function animate() {
 
     var delta = clock.getDelta();
     if ( mixer ) mixer.update( delta );
+
+    if(!mixer || !mixer.time) return;
+
+    const animationTime = mixer.time;
+    const frameIndex = Math.floor(animationTime*FRAME_RATE);
+
+  //  if(mixer && mixer.time)
+    console.log(frameIndex);
+}
+animate();*/
+let get_model = findGetParameter('model');
+
+switch(parseInt(get_model)) {
+    case 1:
+        setVOAudio("./sound/vo1.mp3");
+        break;
+    case 2:
+        setVOAudio("./sound/vo2.mp3");
+        break;
+    case 3:
+        setVOAudio("./sound/vo3.mp3");
+        break;
+    case 4:
+        setVOAudio("./sound/vo4.mp3");
+        break;
+}
+
+function setVOAudio(url) {
+    document.getElementById("vo_a").innerHTML =    `<audio autoplay controls>
+    <source id="vo_input" src="${url}" type="audio/mpeg">
+    </audio>`;
+}
+
+let delta = 0;
+// 30 fps
+let interval = 1 / 240;
+
+function animate() {
+  requestAnimationFrame(animate);
+  delta += clock.getDelta();
+
+   if (delta  > interval) {
+       
+        try {
+            if(mixer && mixer._actions[0] && mixer._actions[0].time) {
+                let currentTime = parseFloat(toFixed(mixer._actions[0].time, 1));
+
+                if(currentTime !== pre_Sec) {
+
+                    switch(parseInt(get_model)) {
+                        case 1:
+                            if(currentTime === 0.5 || currentTime === 3.6 || currentTime === 7) {
+                                lowLag.play("/sound/jump.mp3");
+                            }
+                            else if(currentTime === 7.5) {
+                                lowLag.play("/sound/yay.mp3");
+                            }
+                            break;
+                        case 2:
+                            if(currentTime === 2 || currentTime === 3 || currentTime === 4.2 || currentTime === 4.8) {
+                                lowLag.play("/sound/kick.mp3");
+                            }
+                            else if(currentTime === 6) {
+                                lowLag.play("/sound/yay.mp3");
+                                lowLag.play("/sound/clap.mp3");
+                            }
+                            break;
+                        case 3:
+                            if(currentTime === 3.4 || currentTime === 4 || currentTime === 5.2 || currentTime === 5.8 || currentTime === 7) {
+                                lowLag.play("/sound/blow.mp3");
+                            }
+                            else if(currentTime === 7.5) {
+                                lowLag.play("/sound/yay.mp3");
+                            }
+                            break;
+                        case 4:
+                            if(currentTime === 0) {
+                                lowLag.play("/sound/walk.mp3");
+                            }
+                            break;
+                    }
+
+                    //console.log(currentTime);
+
+                }
+
+                pre_Sec = currentTime;
+            }
+        } catch(err) {
+
+        }
+
+        if ( mixer ) mixer.update( delta );
+        renderer.render( scene, camera );
+	    controls.update();
+
+       delta = delta % interval;
+   }
 }
 animate();
 
-let get_model = findGetParameter('model');
+
 let modelPath = "";
 
 const modelPaths = {
@@ -65,7 +177,7 @@ loader.load(modelPaths[get_model],
 		document.getElementById('loverlay').style.display = "none";
 	
         mixer = new THREE.AnimationMixer(gltf.scene);
-        gltf.animations.forEach((clip) => { console.log(clip); mixer.clipAction(clip).play(); });
+        gltf.animations.forEach((clip) => { mixer.clipAction(clip).play(); });
         gltf.scene.children.forEach(c => { c.frustumCulled = false; c.renderOrder=1;})
 
         gltf.scene.traverse(( object ) => {
@@ -113,3 +225,9 @@ function onWindowResize(){
 function back() {
 	window.location.href = "/3D-Work/index.html";
 }
+
+function toFixed(num, fixed) {
+    var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+    return num.toString().match(re)[0];
+}
+
--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const server = express();
-
-server.use('/', express.static(__dirname + '/public'));
-server.use('/3D-Work', express.static(__dirname + '/public'));
-server.listen(8080);
